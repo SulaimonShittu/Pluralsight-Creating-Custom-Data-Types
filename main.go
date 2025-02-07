5a002b0100d5c9a8eeb6b6794b17a3dfc7276d77
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-02-07 18:56:26 +0100

    _ Interfaces & structs _ Type definitions & aliases

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"Pluralsight-Creating-Custom-Data-Types/organization"
+	"fmt"
+)
 
+func main() {
+	p := organization.NewPerson("Sula", "Shittu")
+	err := p.SetTwitterHandler("@eniolorunmife")
+	fmt.Printf("%T\n", organization.TwitterHandler("test"))
+	if err != nil {
+		fmt.Printf("An error occurred setting the Twitter handler: %s\n", err.Error())
+	}
+	fmt.Println(p.GetTwitterHandler())
+	fmt.Println(p.GetTwitterHandler().RedirectUrl())
+	fmt.Println(p.ID())
+	fmt.Println(p.FullName())
 }
