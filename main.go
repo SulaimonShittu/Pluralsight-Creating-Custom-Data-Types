811656087ad0666cbc18f7f4121358228f012bc4
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-02-08 20:15:42 +0100

    _ Course to be started all over again

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"Pluralsight-Creating-Custom-Data-Types/organization"
-	"fmt"
-)
+// Course to be started again
 
 func main() {
-	p := organization.NewPerson("Sula", "Shittu")
-	err := p.SetTwitterHandler("@eniolorunmife")
-	fmt.Printf("%T\n", organization.TwitterHandler("test"))
-	if err != nil {
-		fmt.Printf("An error occurred setting the Twitter handler: %s\n", err.Error())
-	}
-	fmt.Println(p.GetTwitterHandler())
-	fmt.Println(p.GetTwitterHandler().RedirectUrl())
-	fmt.Println(p.ID())
-	fmt.Println(p.FullName())
+
 }
